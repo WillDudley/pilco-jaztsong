9487f4d4b666585c749ee28bae61d6b3ecee5a20
WillDudley <Will2346@live.co.uk>
2021-12-05 16:07:08 +0000

    add bindings

--- pilco_jaztsong/__init__.py
+++ pilco_jaztsong/__init__.py
@@ -1,1 +1,6 @@
 __version__ = '0.1.0'
+
+from pilco_jaztsong.controllers import LinearController, FakeGPR, RbfController
+from pilco_jaztsong.rewards import ExponentialReward, LinearReward, CombinedRewards
+from pilco_jaztsong.models.pilco import PILCO
+from pilco_jaztsong.models.mgpr import MGPR
